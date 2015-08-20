c547b9021e1f10e98af85a85fc467f469b4ff175
Carlos Villavicencio <carlos.po5i@gmail.com>
2015-08-20 14:56:20 -0300

    -Prueba para subida de archivos desde el dishadmin -Estilo para tabs en request

--- www/js/app.js
+++ www/js/app.js
@@ -10,7 +10,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'ngCart', 'oauthApp.services', 'LocalStorageModule','ionic.rating'])
+angular.module('starter', ['ionic', 'starter.controllers', 'ngCart', 'oauthApp.services', 'LocalStorageModule', 'ionic.rating', 'ngFileUpload'])
 
 .run(function($ionicPlatform, localStorageService, $rootScope, $ionicLoading) {
   $ionicPlatform.ready(function() {
